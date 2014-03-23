1e864c2fab730ae31b9c28ea41908dce0f419537
pippin lee <pippinblee@gmail.com>
2014-03-22 20:34:05 -0400

    make urls get numeric profile of individual fatalities

--- app.py
+++ app.py
@@ -16,7 +16,10 @@ def index():
 
 @app.route('/<number>/')
 def detail(number):
-    return render_template('detail.html')
+	return render_template('detail.html',
+		object=csv_dict[number],
+	)
+
 
 if __name__ == '__main__':
     app.run(
